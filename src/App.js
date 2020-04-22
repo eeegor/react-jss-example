346006e8b0ad3b6aa03bf586bde1a49d121ed88d
Egor <egor.kirpichev@gmail.com>
2020-04-22 15:09:57 +0200

    add color update example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {createUseStyles} from 'react-jss'
+
 import './App.css';
 
+const useStyles = createUseStyles({
+  myInput: {
+    color: ({color}) => color,
+    margin: {
+      // jss-plugin-expand gives more readable syntax
+      top: 5, // jss-plugin-default-unit makes this 5px
+      right: 0,
+      bottom: 0,
+      left: '1rem'
+    },
+    '& span': {
+      // jss-plugin-nested applies this to a child span
+      fontWeight: 'bold' // jss-plugin-camel-case turns this into 'font-weight'
+    }
+  }
+})
+
+const Input = ({value, onChange, theme}) => {
+  const classes = useStyles(theme)
+  return (
+    <input value={value} onChange={onChange} className={classes.myInput} /> 
+  )
+}
+
 function App() {
+  const [value, setValue] = useState('')
+  const [color, setColor] = useState('blue')
+
+  const handleColor = (event) => {
+    event.preventDefault()
+    setColor('pink')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={handleColor}>Change color</button>
+      </div>
+      <div>
+        <Input theme={{color}} value={value} onChange={e => setValue(e.target.value)} />
+      </div>
     </div>
   );
 }
